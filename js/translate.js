8eb7e33afa990c95f8c6784ba0eb0a077b70cc13
sangdo <sang.do@knorex.com>
2023-11-08 21:09:42 +0700

    - auto save writing content

--- js/translate.js
+++ js/translate.js
@@ -1,7 +1,15 @@
+//constants
+const LOCAL_KEY = 'writing_key';
+
 //split essay into isolated lines
 function parse_content(){
+    clearStorage();
     $('.line', $('#tbl_parse')).remove();   //clear existing data
     var content_vi = $('#txt_content_vi').val();
+    content_vi = $.trim(content_vi);
+    if (content_vi == ''){
+        return;
+    }
     var splits_vi = content_vi.split('.');
     var content_en = $('#txt_content_en').val();
     var splits_en = content_en.split('.');
@@ -39,16 +47,44 @@ function toggle_all(){
     }
 }
 //copy typing texts
-function copy_all(){
+function copy_all(is_auto_copy){
     $('#div_copied').hide();
     var all = $('.txt_typing');
     var len = all.length;
+    var content = '';
     if (len > 1){
         var copied_text = [];
         for (var i=1; i<len; i++){
             copied_text.push($.trim($(all[i]).val()));
         }
-        navigator.clipboard.writeText(copied_text.join('. '));
-        $('#div_copied').show();
+        content = copied_text.join('. ');
+        if (content != '' && !is_auto_copy){
+            navigator.clipboard.writeText(content);
+            $('#div_copied').show();
+        }
+    }
+    return content;
+}
+//
+function clearStorage(){
+    localStorage.setItem(LOCAL_KEY, '');
+}
+//
+function saveEditingContent2LocalStore(content){
+    var content = copy_all(true);
+    content = $.trim(content);
+    if (content != ''){
+        localStorage.setItem(LOCAL_KEY, content);
     }
 }
+//
+function restore_local(){
+    var content = localStorage.getItem(LOCAL_KEY, '');
+    if (content == ''){
+        return;
+    }
+}
+//
+$( document ).ready(function() {
+    setInterval(saveEditingContent2LocalStore, 10*1000); //auto save writing content every 10 seconds
+});
